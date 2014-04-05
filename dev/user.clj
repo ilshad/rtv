baaadc885a9b9856ba71eb72733436c234058bfc
Ilshad Khabibullin <astoon@spacta.com>
2014-04-05 17:29:49 +0400

    db schema upgrade (tricks), test data

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,6 @@
 (ns user
   (:require [clojure.repl :refer :all]
+            [clojure.java.io :as io]
             [com.stuartsierra.component :as c]
             [clojure.tools.namespace.repl :as namespace-repl]
             [environ.core :refer [env]]
@@ -12,9 +13,13 @@
 (def conf (select-keys env [:datomic-uri :http-server-port
                             :facebook :aws]))
 
+(defn seed []
+  (-> "test-data.edn" io/resource slurp read-string))
+
 (defn go []
   (alter-var-root #'sys (constantly (system/system conf)))
-  (alter-var-root #'sys c/start))
+  (alter-var-root #'sys c/start)
+  @(d/transact (-> sys :db :conn) (seed)))
 
 (defn stop []
   (alter-var-root #'sys (fn [s] (when s (c/stop s)))))
